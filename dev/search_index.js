a59174c0175e76da867ae989176119d9a0c08788
Documenter.jl <documenter@juliadocs.github.io>
2024-11-16 04:16:25 +0000

    build based on b36dee4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OptimalBranching","category":"page"},{"location":"#OptimalBranching","page":"Home","title":"OptimalBranching","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OptimalBranching.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OptimalBranching]","category":"page"}]
+}
