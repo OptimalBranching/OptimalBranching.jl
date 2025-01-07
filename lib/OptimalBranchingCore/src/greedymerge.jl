78b3d532bd21b6c2d32e88e88a95b40cbcb936f1
Zhongyi <zni573@connect.hkust-gz.edu.cn>
2025-01-07 14:11:05 +0800

    save

--- lib/OptimalBranchingCore/src/greedymerge.jl
+++ lib/OptimalBranchingCore/src/greedymerge.jl
@@ -41,3 +41,57 @@ function greedymerge(cls::Vector{Vector{Clause{INT}}}, problem::AbstractProblem,
     end
     return OptimalBranchingResult(DNF([cl[1] for cl in cls]), size_reductions, γ)
 end
+
+function localsearch(cls::Vector{Vector{Clause{INT}}}, problem::AbstractProblem, variables::Vector, m::AbstractMeasure) where {INT}
+    cls = copy(cls)
+    size_reductions = Float64[size_reduction(problem, m, first(candidate), variables) for candidate in cls]
+    γ = complexity_bv(size_reductions)
+    communities = [[i] for i in 1:length(cls)]
+	mincls = [cls[i][1] for i in 1:length(cls)]
+    for β in 1:1:1000.0
+        # fuse, or free
+        for i ∈ 1:length(communities), j in 1:length(communities)
+            (i == j) && continue
+            # try free
+            for fi in 1:length(communities[i])
+                # bring communities[i][fi] to communities[j]
+                reduction_i,cli = merge_community(problem, m, cls[setdiff(communities[i], communities[i][fi])], variables)
+                reduction_j,clj = merge_community(problem, m, cls[communities[j] ∪ communities[i][fi]], variables)
+				iszero(clj.mask) && continue
+                ΔE = γ^(-reduction_i) + γ^(-reduction_j) - γ^(-size_reductions[i]) - γ^(-size_reductions[j])
+                if rand() < exp(-β * ΔE)
+                    push!(communities[j], communities[i][fi])
+                    deleteat!(communities[i], fi)
+                    size_reductions[i] = reduction_i
+                    size_reductions[j] = reduction_j
+                    γ = complexity_bv(size_reductions[findall(!isempty, communities)])
+					mincls[i] = cli
+					mincls[j] = clj
+                    break
+                end
+            end
+        end
+		@show β
+		@show γ
+    end
+	pos = findall(!isempty, communities)
+    return OptimalBranchingResult(DNF(mincls[pos]), size_reductions[pos], γ)
+end
+
+function merge_community(problem::AbstractProblem, m::AbstractMeasure, cls::Vector{Vector{Clause{INT}}}, variables::Vector) where {INT}
+    isempty(cls) && return Inf,Clause(zero(INT), zero(INT))
+    maxclause = Clause(zero(INT), zero(INT))
+    maxred = 0
+	pos = [length(cls[i]) for i in 1:length(cls)]
+	while true
+		cl = reduce((x,y) ->gather2(length(variables), x, y), [cls[i][pos[i]] for i in 1:length(cls)])
+		reduction = size_reduction(problem, m, cl, variables)
+		if reduction > maxred
+			maxred, maxclause = reduction, cl
+		end
+		posi = findfirst(x -> x > 1, pos)
+		isnothing(posi) && break
+		pos[posi] -= 1
+	end
+    return maxred, maxclause
+end
