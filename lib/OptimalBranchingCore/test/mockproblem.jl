cd886bb796e8f49ae24aa1dafdca6e71088c85b0
Jinguo Liu <cacate0129@gmail.com>
2025-03-06 10:37:34 +0800

    Allow setting initial gamma (#48)
    * allow setting initial gamma
    
    * allow setting initial gamma

--- lib/OptimalBranchingCore/test/mockproblem.jl
+++ lib/OptimalBranchingCore/test/mockproblem.jl
@@ -25,20 +25,31 @@ end
     n = 100
     nsample = 3
     p = MockProblem(rand(Bool, n))
-    config = BranchingStrategy(table_solver=MockTableSolver(nsample), measure=NumOfVariables(), selector=RandomSelector(16))
+    config = BranchingStrategy(table_solver=MockTableSolver(nsample),  measure=NumOfVariables(), selector=RandomSelector(16), set_cover_solver=NaiveBranch())
+    res0 = branch_and_reduce(p, config, NoReducer(), MaxSizeBranchCount; show_progress=false)
+    @test res0.size == 100
+
+    config = BranchingStrategy(table_solver=MockTableSolver(nsample), measure=NumOfVariables(), selector=RandomSelector(16), set_cover_solver=IPSolver())
     @test branch_and_reduce(p, config, NoReducer(), MaxSize).size == 100
     res1 = branch_and_reduce(p, config, NoReducer(), MaxSizeBranchCount; show_progress=true)
     @test res1.size == 100
+    @test res1.count < res0.count
 
-    config = BranchingStrategy(table_solver=MockTableSolver(nsample),  measure=NumOfVariables(), selector=RandomSelector(16), set_cover_solver=NaiveBranch())
-    res2 = branch_and_reduce(p, config, NoReducer(), MaxSizeBranchCount; show_progress=true)
+    config = BranchingStrategy(table_solver=MockTableSolver(nsample),  measure=NumOfVariables(), selector=RandomSelector(16), set_cover_solver=LPSolver())
+    res2 = branch_and_reduce(p, config, NoReducer(), MaxSizeBranchCount; show_progress=false)
     @test res2.size == 100
-    @test res1.count < res2.count
+    @test res2.count < res0.count
 
     config = BranchingStrategy(table_solver=MockTableSolver(nsample),  measure=NumOfVariables(), selector=RandomSelector(16), set_cover_solver=GreedyMerge())
-    res3 = branch_and_reduce(p, config, NoReducer(), MaxSizeBranchCount; show_progress=true)
+    res3 = branch_and_reduce(p, config, NoReducer(), MaxSizeBranchCount; show_progress=false)
     @test res3.size == 100
-    @test res2.count > res3.count
+    @test res3.count < res0.count
 
-    @show res1.count, res2.count, res3.count
+    # gamma informed, let γ0 be 1.05
+    config = BranchingStrategy(table_solver=MockTableSolver(nsample), measure=NumOfVariables(), selector=RandomSelector(16), set_cover_solver=IPSolver(max_itr=1, γ0=1.05))
+    res4 = branch_and_reduce(p, config, NoReducer(), MaxSizeBranchCount; show_progress=false)
+    @test res4.size == 100
+    @test res4.count < res0.count
+
+    @show res0.count, res1.count, res2.count, res3.count, res4.count
 end
