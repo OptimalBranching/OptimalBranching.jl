0c33230027a1e451d755ada98a2e7875a553adc1
ArrogantGao <xz.gao@connect.ust.hk>
2025-03-20 15:47:55 +0800

    add reduce_graph function

--- lib/OptimalBranchingMIS/src/reducer.jl
+++ lib/OptimalBranchingMIS/src/reducer.jl
@@ -69,18 +69,21 @@ The function checks the number of vertices in the graph:
 - For graphs with more than two vertices, it calculates the degrees of the vertices and identifies the vertex with the minimum degree to determine which vertices to remove.
 """
 function OptimalBranchingCore.reduce_problem(::Type{R}, p::MISProblem, reducer::Union{MISReducer, XiaoReducer, TensorNetworkReducer}) where R
-    p_new, r, _ = reduce_problem_vmap(R, p, reducer)
-    return p_new, r
+    g_new, r, _ = reduce_graph(p.g, reducer)
+    if (nv(g_new) == nv(p.g)) && iszero(r)
+        return p, R(0)
+    else
+        return MISProblem(g_new), R(r)
+    end
 end
 
-function reduce_problem_vmap(::Type{R}, p::MISProblem, MISReducer) where R
-    g = p.g
+function reduce_graph(g::SimpleGraph{Int}, ::MISReducer)
     if nv(g) == 0
-        return MISProblem(SimpleGraph(0)), R(0), Int[]
+        return SimpleGraph(0), 0, Int[]
     elseif nv(g) == 1
-        return MISProblem(SimpleGraph(0)), R(1), Int[]
+        return SimpleGraph(0), 1, Int[] 
     elseif nv(g) == 2
-        return MISProblem(SimpleGraph(0)), R(2 - has_edge(g, 1, 2)), Int[]
+        return SimpleGraph(0), 2 - has_edge(g, 1, 2), Int[]
     else
         degrees = degree(g)
         degmin = minimum(degrees)
@@ -89,71 +92,49 @@ function reduce_problem_vmap(::Type{R}, p::MISProblem, MISReducer) where R
         if degmin == 0
             all_zero_vertices = findall(==(0), degrees)
             g_new, vmap = remove_vertices_vmap(g, all_zero_vertices)
-            return MISProblem(g_new), R(length(all_zero_vertices)), vmap
+            return g_new, length(all_zero_vertices), vmap
         elseif degmin == 1
             g_new, vmap = remove_vertices_vmap(g, neighbors(g, vmin) ∪ vmin)
-            return MISProblem(g_new), R(1), vmap
+            return g_new, 1, vmap
         elseif degmin == 2
-            g_new, n, vmap = folding_vmap(g, vmin)
-            return MISProblem(g_new), R(n), vmap
+            return folding_vmap(g, vmin)
         end
     end
-    return p, R(0), collect(1:nv(g))
+    return g, 0, collect(1:nv(g))
 end
 
-function reduce_problem_vmap(::Type{R}, p::MISProblem, ::XiaoReducer) where R
-    g = p.g
-    if nv(g) == 0
-        # NOTE: using NoProblem can be slower due to type instability.
-        return MISProblem(SimpleGraph(0)), R(0), Int[]
-    elseif nv(g) == 1
-        return MISProblem(SimpleGraph(0)), R(1), Int[]
-    elseif nv(g) == 2
-        return MISProblem(SimpleGraph(0)), R(2 - has_edge(g, 1, 2)), Int[]
-    else
-        degrees = degree(g)
-        degmin = minimum(degrees)
-        vmin = findfirst(==(degmin), degrees)
+function reduce_graph(g::SimpleGraph{Int}, ::XiaoReducer)    
+    nv(g) == 0 && return SimpleGraph(0), 0, Int[]
 
-        if degmin == 0
-            all_zero_vertices = findall(==(0), degrees)
-            g_new, vmap = remove_vertices_vmap(g, all_zero_vertices)
-            return MISProblem(g_new), R(length(all_zero_vertices)), vmap
-        elseif degmin == 1
-            g_new, vmap = remove_vertices_vmap(g, neighbors(g, vmin) ∪ vmin)
-            return MISProblem(g_new), R(1), vmap
-        elseif degmin == 2
-            g_new, n, vmap = folding_vmap(g, vmin)
-            return MISProblem(g_new), R(n), vmap
-        end
+    g_new, r, vmap = reduce_graph(g, MISReducer())
+    (g_new != g) && return g_new, r, vmap
 
-        g = copy(p.g)
-
-        unconfined_vs = unconfined_vertices(g)
-        if length(unconfined_vs) != 0
-            @debug "Removing unconfined vertex $(unconfined_vs[1])"
-            g_new, vmap = remove_vertices_vmap(g, [unconfined_vs[1]])
-            return MISProblem(g_new), R(0), vmap
-        end
-
-        twin_res = twin_filter_vmap(g)
-        !isnothing(twin_res) && return MISProblem(twin_res[1]), R(2), twin_res[2]
-        
-        short_funnel_res = short_funnel_filter_vmap(g)
-        !isnothing(short_funnel_res) && return MISProblem(short_funnel_res[1]), R(1), short_funnel_res[2]
-
-        desk_res = desk_filter_vmap(g)
-        !isnothing(desk_res) && return MISProblem(desk_res[1]), R(2), desk_res[2]
+    unconfined_vs = unconfined_vertices(g)
+    if length(unconfined_vs) != 0
+        @debug "Removing unconfined vertex $(unconfined_vs[1])"
+        g_new, vmap = remove_vertices_vmap(g, [unconfined_vs[1]])
+        return g_new, 0, vmap
     end
 
-    return p, R(0), collect(1:nv(g))
+    twin_res = twin_filter_vmap(g)
+    !isnothing(twin_res) && return twin_res[1], 2, twin_res[2]
+    
+    short_funnel_res = short_funnel_filter_vmap(g)
+    !isnothing(short_funnel_res) && return short_funnel_res[1], 1, short_funnel_res[2]
+
+    desk_res = desk_filter_vmap(g)
+    !isnothing(desk_res) && return desk_res[1], 2, desk_res[2]
+
+    return g, 0, collect(1:nv(g))
 end
 
-function reduce_problem_vmap(::Type{R}, p::MISProblem, tnreducer::TensorNetworkReducer) where R
-    rp, reducedvalue, vmap = reduce_problem_vmap(R, p, tnreducer.sub_reducer)
-    (rp.g != p.g) && return rp, reducedvalue, vmap
+function reduce_graph(g::SimpleGraph{Int}, tnreducer::TensorNetworkReducer)
+    nv(g) == 0 && return SimpleGraph(0), 0, Int[]
+    g_new, r, vmap = reduce_graph(g, tnreducer.sub_reducer)
+    (g_new != g) && return g_new, r, vmap
 
     # TODO: consider better strategy for selecting
+    p = MISProblem(g)
     for i in 1:nv(p.g)
         selected_vertices = select_region(p.g, i, tnreducer.n_max, tnreducer.selector)
         truth_table = branching_table(p, TensorNetworkSolver(), selected_vertices)
@@ -164,11 +145,11 @@ function reduce_problem_vmap(::Type{R}, p::MISProblem, tnreducer::TensorNetworkR
             vertices_removed = removed_vertices(selected_vertices, p.g, bc)
             g_new, vmap = remove_vertices_vmap(p.g, vertices_removed)
             reducedvalue = count_ones(bc.val)
-            return MISProblem(g_new), R(reducedvalue), vmap
+            return g_new, reducedvalue, vmap
         end
     end
 
-    return p, R(0), collect(1:nv(p.g))
+    return g, 0, collect(1:nv(g))
 end
 
 function best_intersect(p::MISProblem, tbl::BranchingTable, measure::AbstractMeasure, intersect_strategy::Symbol, variables::Vector{T}) where {T}
