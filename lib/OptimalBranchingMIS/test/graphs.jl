fa905f78aaccd4f3734e491b8b016661e44a4c09
ArrogantGao <xz.gao@connect.ust.hk>
2025-03-25 20:43:30 +0800

    update tests

--- lib/OptimalBranchingMIS/test/graphs.jl
+++ lib/OptimalBranchingMIS/test/graphs.jl
@@ -1,5 +1,5 @@
-using OptimalBranchingCore
-using OptimalBranchingMIS, EliminateGraphs.Graphs
+using OptimalBranchingCore, OptimalBranchingMIS
+using EliminateGraphs, EliminateGraphs.Graphs
 using Test
 
 using OptimalBranchingMIS: neighbor_cover, reduced_alpha, reduced_alpha_configs, graph_from_tuples, collect_configs
@@ -54,4 +54,37 @@ end
             @test nn == OptimalBranchingMIS.open_neighbors(sub_g, n)
         end
     end
+end
+
+@testset "kernelize" begin
+    function kernelize(g::SimpleGraph, reducer::TensorNetworkReducer; verbose::Int = 0, vmap::Vector{Int} = collect(1:nv(g)))
+        (verbose ≥ 2) && (@info "kernelizing graph: $(nv(g)) vertices, $(ne(g)) edges")
+        r = 0
+    
+        vmap_0 = vmap
+    
+        while true
+            res = OptimalBranchingMIS.reduce_graph(g, reducer, vmap_0 = vmap_0) # res = (g_new, r_new, vmap_new)
+            vmap_0 = res[3]
+            vmap = vmap[res[3]]
+            r += res[2]
+            if g == res[1]
+                (verbose ≥ 2) && (@info "kernelized graph: $(nv(g)) vertices, $(ne(g)) edges")
+                return (g, r, vmap)
+            end
+            g = res[1]
+        end
+    end
+
+    g = random_regular_graph(100, 3)
+    reducer = TensorNetworkReducer()
+    gk, r, _ = kernelize(g, reducer)
+    @test nv(gk) ≤ nv(g)
+    @test nv(gk) == length(reducer.region_list)
+
+    @test mis2(EliminateGraph(gk)) + r == mis2(EliminateGraph(g))
+    
+    reducer = TensorNetworkReducer()
+    gkk, _, _ = kernelize(gk, reducer)
+    @test gkk == gk
 end
