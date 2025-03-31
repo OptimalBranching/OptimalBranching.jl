c1256b5172709b633f865a980835e3634278a912
GiggleLiu <cacate0129@gmail.com>
2025-04-01 00:18:05 +0800

    save

--- dataset/gen_ob.jl
+++ dataset/gen_ob.jl
@@ -2,14 +2,14 @@ using OptimalBranchingCore
 using OptimalBranchingCore: NumOfVariables, MockProblem, MockTableSolver, IPSolver, optimal_branching_rule, BitBasis
 using JSON3
 
-function gen_ob(n::Int, nstrings::Int, output_file::String, solver, nsample::Int)
+function gen_ob(n::Int, nstrings::Int, output_file::String, solver, nsample::Int, p::Float64)
     output_data = []
     for i in 1:nsample
         @info "Generating sample $i"
-        p = MockProblem(rand(Bool, n))
-        table_solver = MockTableSolver(nstrings, 0.2)
-        tbl = branching_table(p, table_solver, 1:n)
-        r1 = optimal_branching_rule(tbl, collect(1:n), p, NumOfVariables(), solver)
+        problem = MockProblem(rand(Bool, n))
+        table_solver = MockTableSolver(nstrings, p)
+        tbl = branching_table(problem, table_solver, 1:n)
+        r1 = optimal_branching_rule(tbl, collect(1:n), problem, NumOfVariables(), solver)
         # Convert the optimal rule to a serializable format
         item = Dict(
             "input_table" => table2dict(tbl),
@@ -39,15 +39,15 @@ function clause2dict(n::Int, clause::Clause)
 end
 
 # Generate and save to file
-function gen_ob_and_save(n, nstrings, nsample)
-    output_file = joinpath(@__DIR__, "optimal_branching_result-n=$n-nstrings=$nstrings.json")
-    gen_ob(n, nstrings, output_file, IPSolver(), nsample)
+function gen_ob_and_save(n, nstrings, nsample, p)
+    output_file = joinpath(@__DIR__, "optimal_branching_result-n=$n-nstrings=$nstrings-p=$p.json")
+    gen_ob(n, nstrings, output_file, IPSolver(), nsample, p)
     @info("Results saved to $output_file")
     return output_file
 end
 
-n, nstrings = 15, 40
-output_file = gen_ob_and_save(n, nstrings, 1000)
+n, nstrings = 5, 5
+output_file = gen_ob_and_save(n, nstrings, 1000, 0.0)
 
 function load_ob(input_file::String)
     # Read the JSON file
