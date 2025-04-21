a4724227d81f5f03cceeeee6e9f07e66bc87d374
GiggleLiu <cacate0129@gmail.com>
2025-04-21 21:16:06 +0800

    simplify

--- dataset/gen_mis.jl
+++ dataset/gen_mis.jl
@@ -1,23 +1,33 @@
 # Generate end to end dataset for MIS
 using OptimalBranchingCore, OptimalBranchingMIS
-using OptimalBranchingCore: IPSolver, optimal_branching_rule, BitBasis
+using OptimalBranchingCore: IPSolver, optimal_branching_rule, BitBasis, NumOfVariables
 using OptimalBranchingMIS.GenericTensorNetworks: random_diagonal_coupled_graph
 using Graphs
 using JSON3
 
-function gen_mis(graph::SimpleGraph, output_file::String)
+function OptimalBranchingCore.size_reduction(p::AbstractProblem, m::NumOfVariables, cl::Clause{INT}, variables::Vector) where {INT}
+    return count_ones(cl.mask)
+end
+function gen_mis(graph::SimpleGraph, output_file::String, single_column::Bool)
     problem = MISProblem(graph)
     output_data = []
     for i in 1:nv(graph)
         @info "Generating sample $i"
         variables, openvars = OptimalBranchingMIS.neighbor_cover(graph, i, 2)
         tbl = branching_table(problem, TensorNetworkSolver(; prune_by_env=true), variables)      # compute the BranchingTable
+        if single_column
+            for row in tbl.table
+                resize!(row, 1)
+            end
+        end
         @show length(tbl.table)
         if length(tbl.table) > 100
             @warn "Branching table is too large: $length(tbl.table)"
             continue
         end
-        result = optimal_branching_rule(tbl, variables, problem, D3Measure(), IPSolver())        # compute the optimal branching rule
+        # measure = D3Measure()
+        measure = NumOfVariables()
+        result = optimal_branching_rule(tbl, variables, problem, measure, IPSolver())        # compute the optimal branching rule
 
         # save the environment data (up to 4-layer neighbors)
         variables4, openvars4 = OptimalBranchingMIS.neighbor_cover(graph, i, 4)
@@ -65,21 +75,21 @@ function gen_renyi_and_save(n, p)
     output_file = joinpath(@__DIR__, "mis_result-renyi-n=$n-p=$p.json")
     # generate a Erdos-Renyi graph with n vertices and edge probability p
     graph = erdos_renyi(n, p)
-    gen_mis(graph, output_file)
+    gen_mis(graph, output_file, true)
     @info("Results saved to $output_file")
     return output_file
 end
 function gen_ksg_and_save(L::Int)
     ksg = random_diagonal_coupled_graph(L, L, 0.8)
     output_file = joinpath(@__DIR__, "mis_result-ksg-L=$L.json")
-    gen_mis(SimpleGraph(ksg), output_file)
+    gen_mis(SimpleGraph(ksg), output_file, true)
     @info("Results saved to $output_file")
     return output_file
 end
 function gen_regular3_and_save(n::Int)
     output_file = joinpath(@__DIR__, "mis_result-regular3-n=$n.json")
     graph = random_regular_graph(n, 3)
-    gen_mis(graph, output_file)
+    gen_mis(graph, output_file, true)
 end
 # output_file = gen_ksg_and_save(32)
 # output_file = gen_renyi_and_save(1000, 0.005)
